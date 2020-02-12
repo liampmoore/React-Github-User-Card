8bdcee051460962083c656bab159c3e4bdd1dd49
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-02-11 19:51:58 -0500

    Updates

---
+++ precache-manifest.32c29466375bf1aea3892772efca22b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d424fd1ae64df5cf089cf1d9cc84b08c",
+    "url": "/React-Github-User-Card/index.html"
+  },
+  {
+    "revision": "0c405d63e0c80b24ea45",
+    "url": "/React-Github-User-Card/static/css/main.0a12a10d.chunk.css"
+  },
+  {
+    "revision": "0c4086c9e9df1a1a96d1",
+    "url": "/React-Github-User-Card/static/js/2.824c9248.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Github-User-Card/static/js/2.824c9248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c405d63e0c80b24ea45",
+    "url": "/React-Github-User-Card/static/js/main.6c454686.chunk.js"
+  },
+  {
+    "revision": "549f3d131ff80892e861",
+    "url": "/React-Github-User-Card/static/js/runtime-main.137d5551.js"
+  }
+]);
